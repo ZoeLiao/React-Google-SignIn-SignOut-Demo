be552dc4c3ae3541b1c9786299930a5a1996e869
ZoeLiao <zoejoyuliao@gmail.com>
2020-07-10 23:28:45 +0800

    Modify src/GoogleBtn.js ro replace response.Zi.access_token with response.accessToken

--- src/GoogleBtn.js
+++ src/GoogleBtn.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 
@@ -22,10 +21,10 @@ class GoogleBtn extends Component {
   }
 
   login (response) {
-    if(response.Zi.access_token){
+    if(response.accessToken){
       this.setState(state => ({
         isLogined: true,
-        accessToken: response.Zi.access_token
+        accessToken: response.accessToken
       }));
     }
   }
